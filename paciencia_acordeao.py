327b81cb9c2d016678ba28b90401aeee19206229
marlonsp <marlonsp@al.insper.edu.br>
2021-04-28 11:38:11 -0300

    Codigo geral v2

--- paciencia_acordeao.py
+++ paciencia_acordeao.py
@@ -10,7 +10,7 @@ print('=-='*8)
 
 Jogar = str(input('Aperte Enter para iniciar o jogo: ')) #start do jogo
 
-if Jogar == '':
+while Jogar == '':
     baralho = cria_baralho()
     print('Estado atual do baralho: ')
     n = 1 #númerador para as cartas
@@ -20,7 +20,7 @@ if Jogar == '':
     continua = pmp(baralho)
     while continua:
         print('')
-        movimento = int(input('Escolha o número de uma carta para movê-la: '))
+        movimento = int(input('Escolha o número de uma carta para movê-la (Entre 1 e {}): '.format(len(baralho))))
         while movimento not in range(1, 53):
             movimento = int(input('Esse não um número válido, escolha outro: '))
         while len(lmp(baralho, movimento-1)) == 0:
@@ -31,19 +31,33 @@ if Jogar == '':
             else:
                 baralho = empilha(baralho, movimento-1, movimento-4)
         else:
-            escolha_de_movimento = int(input('As posições possíveis para mover essa carta são: {0} e {1}, reescreva o número para movê-la: '.format(movimento-1,movimento-3)))
-            baralho = empilha(baralho, movimento-1, escolha_de_movimento-1)
+            print('')
+            print('Essa carta possui duas opções de movimentos: ')
+            print('1- {}'.format(baralho[movimento-2]))
+            print('2- {}'.format(baralho[movimento-4]))
+            escolha_de_movimento = int(input('Qual sua escolha? (1 ou 2): '))
+            if escolha_de_movimento == 1:
+                baralho = empilha(baralho, movimento-1, movimento-2)
+            else:
+                baralho = empilha(baralho, movimento-1, movimento-4)       
+        print('')
         n = 1 #númerador para as cartas
         print('Estado atual do baralho: ')
         for i in baralho:
             print('{0}- {1}'.format(n, i))
             n += 1
         continua = pmp(baralho)
-
     print('Fim de jogo')
     if len(baralho) == 1:
         print('Você venceu')
     else:
         ('Você perdeu')
+    quer_novamente = str(input('Deseja jogar novamente? (S/N) '))
+    while quer_novamente != 'S' or quer_novamente != 's' or quer_novamente != 'N' or quer_novamente != 'n':
+        quer_novamente = str(input('Essa não é uma resposta válida, deseja jogar novamente? (S/N) '))
+    if quer_novamente == 'S' or quer_novamente == 's':
+        Jogar = ''
+    else:
+        Jogar = 'fim'
 else:
     print('Fim de jogo')
