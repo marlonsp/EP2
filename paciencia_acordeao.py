7bd55eeb247da155ceea7aaccc07fc8c04377bc4
marlonsp <marlonsp@al.insper.edu.br>
2021-05-05 19:35:31 -0300

    Novas funções e verificação de input

--- paciencia_acordeao.py
+++ paciencia_acordeao.py
@@ -1,15 +1,21 @@
+#funções para o funcionamento do jogo
 from f_cria_baralho import cria_baralho
 from f_extrai_valor import extrai_valor
 from f_extrai_naipe import extrai_naipe
 from movimentos_possiveis import lista_movimentos_possiveis
 from possibilidade_de_movimento import possui_movimentos_possiveis
 from empilha_carta import empilha
+#Funções para validar o input
+from validacao import valida_inteiro
+from validacao import valida_faixa
 
+#inicio do jogo
 print('Paciencia Acordeão')
 print('=-='*8)
 
 Jogar = str(input('Aperte Enter para iniciar o jogo: '))  # start do jogo
 
+#verificador de start
 while Jogar == '':
     baralho = cria_baralho()
     #Print do baralho
@@ -23,26 +29,28 @@ while Jogar == '':
     #Se existirem jogadas possíveis, o jogo continua
     while continua:
         print('')
-        movimento = int(input(
-            'Escolha o número de uma carta para movê-la (Entre 1 e {}): '.format(len(baralho))))
-        while movimento not in range(1, 53):
-            movimento = int(
-                input('Esse não um número válido, escolha outro: '))
-        while len(lista_movimentos_possiveis(baralho, movimento-1)) == 0:
-            movimento = int(
-                input('Essa carta não possui movimentos disponíveis, escolha outra: '))
+        movimento = input('Escolha o número de uma carta para movê-la (Entre 1 e {}): '.format(len(baralho)))
+        # Verifica se a entrada para o movimento é válida
+        while valida_faixa(1, len(baralho), movimento) == False:  
+                movimento = input('Isso não é um número válido , escolha outro: ')
+        #empilha carta com apenas 1 opção de movimento
+        movimento = int(movimento)
         if len(lista_movimentos_possiveis(baralho, movimento-1)) == 1:
             if lista_movimentos_possiveis(baralho, movimento-1)[0] == 1:
                 baralho = empilha(baralho, movimento-1, movimento-2)
             else:
                 baralho = empilha(baralho, movimento-1, movimento-4)
+        #Escolha e empilha carta com 2 opções de movimento
         else:
             print('')
             print('Essa carta possui duas opções de movimentos: ')
-            print('1- {}'.format(baralho[movimento-2]))
-            print('2- {}'.format(baralho[movimento-4]))
-            escolha_de_movimento = int(input('Qual sua escolha? (1 ou 2): '))
-            if escolha_de_movimento == 1:
+            print('1- {}'.format(baralho[movimento-4]))
+            print('2- {}'.format(baralho[movimento-2]))
+            escolha_de_movimento = input('Qual sua escolha? (1 ou 2): ')
+            #valida a escolha de movimento
+            while escolha_de_movimento != '1' and escolha_de_movimento != '2':
+                escolha_de_movimento = input('Isso não é um movimento válido , escolha outro (1 ou 2): ')
+            if escolha_de_movimento == '1':
                 baralho = empilha(baralho, movimento-1, movimento-2)
             else:
                 baralho = empilha(baralho, movimento-1, movimento-4)
@@ -56,18 +64,21 @@ while Jogar == '':
         #Verifica se existem jogadas possíveis
         continua = possui_movimentos_possiveis(baralho)
     print('')
+    #Print do resultado do jo
     if len(baralho) == 1:
         print('Fim de jogo: Você venceu!')
     else:
         print('Fim de jogo: Você perdeu')
+    #Restart do jogo
     quer_novamente = str(input('Deseja jogar novamente? (S/N) '))
+    #Valida a escolha
     while quer_novamente != 'S' and quer_novamente != 's' and quer_novamente != 'N' and quer_novamente != 'n':
-        quer_novamente = str(
-            input('Essa não é uma resposta válida, deseja jogar novamente? (S/N) '))
+        quer_novamente = str(input('Essa não é uma resposta válida, deseja jogar novamente? (S/N) '))
     if quer_novamente == 'S' or quer_novamente == 's':
         Jogar = ''
     else:
         Jogar = 'fim'
+#Encerramento final do jogo
 else:
     print('=-='*8)
     print('Fim de jogo')
---
+++ validacao.py
@@ -0,0 +1,12 @@
+def valida_inteiro(string):
+    v = "0123456789"
+    for letra in string:
+        if letra not in v:
+            return False
+    return True
+
+def valida_faixa(a, b, string):
+    if not valida_inteiro(string):
+        return False
+    valor = int(string)
+    return valor>=a and valor<=b
